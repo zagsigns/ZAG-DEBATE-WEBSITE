5423caeea5e07d0ba65bae0c5277cefe8a469416
zagsigns <zagsigns@gmail.com>
2025-10-14 17:30:56 +0530

    today

--- debate_platform/frontend/src/main.jsx
+++ debate_platform/frontend/src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+// /src/main.jsx
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Import global styles
+
+// Use the createRoot API to render your App component into the 'root' element
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-);
+)
